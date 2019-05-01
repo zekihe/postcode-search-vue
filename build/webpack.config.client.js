6e86cbaeb701a7c1bee7c15f59387c26b07c638f
zekihe <zekihe@163.com>
2019-05-01 21:01:41 +0800

    更新组件内容

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -52,9 +52,9 @@ const devServer = {
           target: 'http://cpdc.chinapost.com.cn:80/web',
           secure: false, // 如果是https接口，需要配置这个参数
           changeOrigin: true,// 如果接口跨域，需要进行这个参数配置
-//           pathRewrite: {   // 如果接口本身没有/api需要通过pathRewrite来重写了地址
-// 　　　　　    '^post': ''
-//           }
+          pathRewrite: {   // 如果接口本身没有/api需要通过pathRewrite来重写了地址
+　　　　　    '^post': ''
+          }
       }
   }
 }
